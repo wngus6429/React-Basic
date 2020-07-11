f6d8001706324b1e3ff6189eddbf5241ec0a0fac
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-07-12 04:07:25 +0900

    712
    712

---
+++ velopert/App2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  // const name = "리액트";
+  // return <div> {name === "리액트" && <h1>리액트입니다</h1>}</div>;
+  const name = "리액트";
+  return <div style={{ backgroundColor: "black", fontSize: 40, color: "white" }}>{name}</div>;
+}
+
+export default App;
